6eef666f3805ce994cd12f71c213cc69ced074e9
grzes <grzegorzskorupko@gmail.com>
2018-06-17 13:52:32 +0200

    first working version with chat database

--- alligator/chat/views.py
+++ alligator/chat/views.py
@@ -5,12 +5,15 @@ from django.http import HttpResponse
 
 from .models import User, Conversation, Message
 
-def index(request):
-    return HttpResponse("Hello, world.")
+class IndexView(generic.ListView):
+    template_name = 'chat/index.html'
+    context_object_name = 'conversations_list'
+    def get_queryset(self):
+        return Conversation.objects.order_by('-name')
 
 class ConversationView(generic.ListView):
     template_name = 'chat/conv.html'
-    context_object_name = 'message_list'
+    context_object_name = 'messages_list'
 
     def get_queryset(self):
-        return Conversation.objects.order_by('-pub_date')
+        return Message.objects.order_by('-pub_date')
